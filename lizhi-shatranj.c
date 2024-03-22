baf3c1145ff99c469c6616943d48edfa47628165
Rain <therainy.dev@tutanota.de>
2024-03-22 21:00:07 +0000

    add cat

--- lizhi-shatranj.c
+++ lizhi-shatranj.c
@@ -21,6 +21,8 @@ warranties of merchantability, fitness for a particular purpose, and noninfringe
 copyright holders be liable for any claim, damages, or other liability, whether in an action of contract, tort, or
 otherwise, arising from, out of, or in connection with the software or the use or other dealings in the software.
 
+ᓚᘏᗢ
+
 */
 
 /* preprocessor stuff -----------------------------------------------------------------------------------------------*/
