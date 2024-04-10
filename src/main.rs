c4d4821d39a85b1f2f412b6d2acf8281708cad48
Rain <therainy.dev@tutanota.de>
2024-04-09 17:13:41 -0700

    uci + cat

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
 fn main() {
 	print!(concat!(
-		"uci\n",
-		"id name lizhi 0.1.0\n",
-		"option name UCI_Variant type combo default shatranj var shatranj\n",
-		"uciok\n"
+		"\x1b[0;3;31mlizhi 0.1.0\x1b[0m by \x1b[32mthe\x1b[36mrainy\x1b[34mdev\x1b[0m\n",
+		"This program is a text-only shatranj engine, but tries to be nice to work with even without a GUI.\n",
+		"If you want to know how to use this program, enter 'help'.\n",
+		"If you need to work directly with UCI, enter 'uci'.\n"
 	));
+
+	loop {
+		print!("");
+
+		let mut input = String::new();
+		std::io::stdin().read_line(&mut input).expect("failed to read input");
+
+		match input.trim() {
+			"cat" => println!("ᓚᘏᗢ"),
+			"isready" => println!("readyok"),
+			"quit" => break,
+			"uci" =>
+				println!(concat!(
+					"uci\n",
+					"id name lizhi 0.1.0\n",
+					"option name UCI_Variant type combo default shatranj var shatranj\n",
+					"uciok"
+				)),
+			_ => println!("\x1b[0;31minvalid command\x1b[0m"),
+		}
+	}
 }
