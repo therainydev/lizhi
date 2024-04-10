8148c009062a907ce8e5e43f2f0c063c91a115c8
小雨 <therainy.dev@tutanota.de>
2024-04-10 19:03:08 +0000

    pseudocode -> real rust, add probably-working evaluation

--- src/search.rs
+++ src/search.rs
@@ -1,8 +1,30 @@
 use std::cmp;
 use cozy_chess;
 
-fn negamax(node: Board, depth: u64, evaluate: fn(position)->u64) -> u64 {
-	if (depth == 0 /* || terminal node */) {
+fn evaluate(position: Board) -> u64 {
+	1000 * (
+		position.colored_pieces(position.side_to_move(), Piece::Pawn)
+		+ 3 * position.colored_pieces(position.side_to_move(), Piece::Knight)
+		+ 3 * position.colored_pieces(position.side_to_move(), Piece::Bishop)
+		+ 5 * position.colored_pieces(position.side_to_move(), Piece::Rook)
+		+ 9 * position.colored_pieces(position.side_to_move(), Piece::Queen)
+		- position.colored_pieces(position.side_to_move.not(), Piece::Pawn)
+		- 3 * position.colored_pieces(position.side_to_move.not(), Piece::Knight)
+		- 3 * position.colored_pieces(position.side_to_move.not(), Piece::Bishop)
+		- 5 * position.colored_pieces(position.side_to_move.not(), Piece::Rook)
+		- 9 * position.colored_pieces(position.side_to_move.not(), Piece::Queen)
+	)
+}
+
+fn negamax(node: Board, depth: u64, evaluate: fn(Board)->u64) -> u64 {
+	let mate_value = 1000000;
+	if (node.status() == GameStatus::Won) {
+		-mate_value
+	}
+	if (node.status() == GameStatus::Drawn) {
+		0
+	}
+	if (depth == 0) {
 		evaluate(node)
 	}
 	let mut evaluation = -2 * mate_value;
